9a2df00405ee5ca8f55fc1dc0c040a267414fa04
APA\SESA489434 <deep.hazra@non.schneider-electric.com>
2019-03-15 10:33:10 +0530

    Fiew minor changes

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-//interface for columns
+// interface for columns
 interface Column {
   headerName: string;
   field: string;
-  sortState?:boolean;
-  sort?:boolean;
-  filter?:boolean;
+  sortState?: boolean;
+  sort?: boolean;
+  filter?: boolean;
 }
 
-//interface for each table row
-interface TableRow{
-  filtered:boolean,
-  data:Array<any>
+// interface for each table row
+interface TableRow {
+  filtered: boolean;
+  data: Array<any>;
 }
 
 @Component({
@@ -23,36 +23,46 @@ interface TableRow{
 export class DataTableComponent implements OnInit {
 
 
-  columnDefs :Column[]= [
-    {headerName: 'Model', field: 'model', sort:true, filter:true },
-    {headerName: 'Make', field: 'make', filter:true },
+  columnDefs: Column[] = [
+    {headerName: 'Model', field: 'model', sort: true, filter: true},
+    {headerName: 'Make', field: 'make', filter: true},
     {headerName: 'Price', field: 'price'}
   ];
 
   rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000},
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000},
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000},
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000}
   ];
 
-  //Convert row data to a 2D array.
-  createTableData(){
-    if(this.columnDefs.length!= Object.keys(this.rowData[0]).length){
-      console.warn("Invalid data: Total Column in def: "+ this.columnDefs.length +"Total Columns in data:" + Object.keys(this.rowData[0]).length);
-    }
-    for(var j=0;j<this.rowData.length;++j){
-      var row:TableRow ={data:[],filtered:false};
+  TableRows: TableRow[] = [];
 
-      for(var i=0;i<this.columnDefs.length;++i){
+
+  constructor() {
+
+    let that = this;
+    this.createTableData();
+  }
+
+  // Convert row data to a 2D array.
+  createTableData() {
+    if (this.columnDefs.length !== Object.keys(this.rowData[0]).length) {
+      console.warn('Invalid data: Total Column in def: ' + this.columnDefs.length + 'Total Columns in data:'
+        + Object.keys(this.rowData[0]).length);
+    }
+    for (let j = 0; j < this.rowData.length; ++j) {
+      let row: TableRow = {data: [], filtered: false};
+
+      for (let i = 0; i < this.columnDefs.length; ++i) {
 
         this.columnDefs[i].sortState = null;
         row.data.push(this.rowData[j][this.columnDefs[i].field])
@@ -61,57 +71,51 @@ export class DataTableComponent implements OnInit {
     }
   }
 
-//Filters data based on CONTAINS.
-  filter(column, text){
+// Filters data based on CONTAINS.
+  filter(column, text) {
 
-    for(var i=0;i<this.TableRows.length;++i){
+    for (let i = 0; i < this.TableRows.length; ++i) {
       this.TableRows[i].filtered = !this.TableRows[i].data[column].includes(text)
     }
   }
-  //Sort columns
-  sortColumn(column){
-    //check whether it is sortable.
-    if(!this.columnDefs[column].sort) return;
-    //Reset all other sort
-    for(var i=0;i<this.columnDefs.length; ++i){
-      if(i==column) continue;
+
+  // Sort columns
+  sortColumn(column) {
+    // check whether it is sortable.
+    if (!this.columnDefs[column].sort) return;
+    // Reset all other sort
+    for (let i = 0; i < this.columnDefs.length; ++i) {
+      if (i === column) continue;
       this.columnDefs[i].sortState = null;
     }
 
-    //cache the sort state
-    var sortState:boolean = this.columnDefs[column].sortState;
-    if(sortState == null) {
+    // cache the sort state
+    let sortState: boolean = this.columnDefs[column].sortState;
+    if (sortState == null) {
       this.columnDefs[column].sortState = true;
     }
     else {
       this.columnDefs[column].sortState = !this.columnDefs[column].sortState;
     }
     sortState = this.columnDefs[column].sortState;
-    var that = this;
-    //Sort te table.
-    this.TableRows.sort(function(a,b){
-        return that.sortFunction(a,b, column, sortState);
+    let that = this;
+    // Sort te table.
+    this.TableRows.sort(function (a, b) {
+      return that.sortFunction(a, b, column, sortState);
     })
   }
-//Sort function
- private sortFunction(a, b, columnValue, isAsc) {
-  if (a.data[columnValue] === b.data[columnValue]) {
-    return 0;
-  }
-  else if(isAsc) {
-    return (a.data[columnValue] < b.data[columnValue]) ? -1 : 1;
-  }
-  else{
-    return (a.data[columnValue] > b.data[columnValue]) ? -1 : 1;
-  }
-}
 
-  TableRows:TableRow[]=[];
-  constructor()
-  {
-
-    var that = this;
-    this.createTableData();
+// Sort function
+  private sortFunction(a, b, columnValue, isAsc) {
+    if (a.data[columnValue] === b.data[columnValue]) {
+      return 0;
+    }
+    else if (isAsc) {
+      return (a.data[columnValue] < b.data[columnValue]) ? -1 : 1;
+    }
+    else {
+      return (a.data[columnValue] > b.data[columnValue]) ? -1 : 1;
+    }
   }
 
   ngOnInit() {

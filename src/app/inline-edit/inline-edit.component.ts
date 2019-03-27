e50bff4500ee3c7f806f0eb1f5d0e17a88b6bfdc
sowvikr <sowvik.roy@hitachivantara.com>
2019-03-27 17:20:19 +0530

    Edit box

--- src/app/inline-edit/inline-edit.component.ts
+++ src/app/inline-edit/inline-edit.component.ts
@@ -23,28 +23,25 @@ interface ContextMenuArgs {
 
 export class InlineEditComponent implements OnInit {
 
-  @Input() row: number;
-  @Input() column: number;
-  @Input() cellData: any;
-  @Input() cellValue: any;
-  @Input() columnDefs: any;
-  @Input() renderer: any;
-  @Input() isEditable: boolean;
-  @Input() isSelected: boolean;
+  @Input() row:number;
+  @Input() column:number;
+  @Input() cellData:any;
+  @Input() cellValue:any;
+  @Input() columnDefs:any;
+  @Input() renderer:any;
+  @Input() isEditable:boolean;
+  @Input() isSelected:boolean;
   @Output() changed = new EventEmitter<any>();
   @Output() rightClicked = new EventEmitter<ContextMenuArgs>();
   @Input() theme:string;
 
-  @ViewChild('name') vc: ElementRef;
+  @ViewChild('name') vc:ElementRef;
 
   private isEdit = false;
   private isCombobox = false;
-  private changedValue: ChangedValue = {column: 0, row: 0, value: null};
-  private contextMenuArgs: ContextMenuArgs = {x: 0, y: 0, isEdit: false, column: 0, row: 0};
+  private changedValue:ChangedValue = {column: 0, row: 0, value: null};
+  private contextMenuArgs:ContextMenuArgs = {x: 0, y: 0, isEdit: false, column: 0, row: 0};
 
-  onFocusOut() {
-    this.isEdit = false;
-  }
 
   onClick() {
     if (this.isEdit) {
@@ -58,7 +55,7 @@ export class InlineEditComponent implements OnInit {
     this.vc.nativeElement.focus();
   }
 
-  onChange(value: string) {
+  onChange(value:string) {
     this.changedValue.value = value;
     this.changedValue.row = this.row;
     this.changedValue.column = this.column;
@@ -72,6 +69,10 @@ export class InlineEditComponent implements OnInit {
     this.contextMenuArgs.row = this.row;
     this.contextMenuArgs.column = this.column;
     this.rightClicked.emit(this.contextMenuArgs);
+  }
+
+  onFocusOut($event) {
+    this.isEdit = false;
   }
 
   constructor() {

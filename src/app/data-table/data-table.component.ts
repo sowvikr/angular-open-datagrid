41904542f92391c1a19a0e22802f01ec6d7e59f0
sowvikr <sowvik.roy@hitachivantara.com>
2019-04-28 15:46:46 +0530

    Fixes #2

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -190,6 +190,9 @@ export class DataTableComponent implements OnInit {
       this.selectRows(event.target.checked, null, true);
       return;
     }
+    if(!event.value){
+      this.selectAllRows = false;
+    }
     this.PagedRows[event.row].rowSelect = event.value;
     this.selectRows(event.value, event.row);
   }
@@ -410,6 +413,10 @@ export class DataTableComponent implements OnInit {
 
   dropRow(event) {
     moveItemInArray(this.PagedRows, event.previousIndex, event.currentIndex);
+    this.allRows(false);
+    this.contextMenuData = [];
+    this.selectAllRows = false;
+
   }
 
   valueChanged(changeValue:any) {
